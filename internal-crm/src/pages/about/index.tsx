a00478a347e292546b774daaa707afa982744c9e
zahidkhan-xen <zk8127194@gmail.com>
2024-10-10 14:34:37 +0500

    first commit

---
+++ internal-crm/src/pages/about/index.tsx
@@ -0,0 +1,11 @@
+// src/pages/about.tsx
+import React from 'react';
+import About from '../components/About';
+
+const AboutPage: React.FC = () => {
+  return (
+    <About />
+  );
+};
+
+export default AboutPage;
---
+++ internal-crm/src/pages/index.tsx
@@ -0,0 +1,31 @@
+import localFont from "next/font/local";
+import { useRouter } from "next/router";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export default function Home() {
+
+  const router = useRouter();
+  return (
+    <div
+      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+    >
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+       <h1> This is me Hello World !</h1>
+       <button onClick={()=> router.push("/about")}>About Page</button>
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+       
+      </footer>
+    </div>
+  );
+}
